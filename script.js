0582d02ef4dea98571e99193fa778b19a72fc3a7
EliezerDevs <64181960+EliezerDevs@users.noreply.github.com>
2022-07-22 12:10:51 -0400

    Minor Changes

--- script.js
+++ script.js
@@ -6,30 +6,30 @@ function convertValue() {
   let length = x.length;
   let aux = length;
   let result = 0;
-  let menorQue8 = false;
+  let lessThan8 = false;
   if(length == 0){
-      alert('Ingrese un valor para continuar');
+      alert('Enter a value to continue');
   }
   else if(length <= 8 && length > 0){
-    menorQue8 = true;
+    lessThan8 = true;
     
   }
   else{
-    alert('El binario que ingreso no esta permitido: (la cantidad de valores deben ser menor o igual a 8)');
+    alert('The binary you entered is not allowed: (the number of values must be less than or equal to 8)');
   }
   
   if (select === 'decimal') {
-    while(menorQue8){
+    while(lessThan8){
       for(let i = 0; i < aux; i++) {
         result = result + (+x[i] * Math.pow(2,length-1));
         if(+x[i] !== 1 && +x[i] !== 0){
-          alert('El binario que ingreso no esta permitido: (Ingrese un binario valido)');
+          alert('The binary you entered is not allowed: (Enter a valid binary)');
           result = 0;
           break;
         }
         length--;
       }
-      menorQue8 = false;
+      lessThan8 = false;
     }
   }
   else {
